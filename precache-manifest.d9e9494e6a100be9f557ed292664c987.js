183caceb8711a0e369f74159827e17703db906c6
Neil Solomon <neilsolomon89@gmail.com>
2020-04-12 16:51:59 -0400

    Updates

---
+++ precache-manifest.d9e9494e6a100be9f557ed292664c987.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34234328a13c3b36b0e33efb9fb8629",
+    "url": "/neil-solomon/csc212-music-playlist/index.html"
+  },
+  {
+    "revision": "8cc08a005a889af88172",
+    "url": "/neil-solomon/csc212-music-playlist/static/css/main.5b79b0f0.chunk.css"
+  },
+  {
+    "revision": "b7bbb6b6f09a4629529a",
+    "url": "/neil-solomon/csc212-music-playlist/static/js/2.232f5185.chunk.js"
+  },
+  {
+    "revision": "be5036304af653a863967da2b97f97a8",
+    "url": "/neil-solomon/csc212-music-playlist/static/js/2.232f5185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc08a005a889af88172",
+    "url": "/neil-solomon/csc212-music-playlist/static/js/main.5f0d8682.chunk.js"
+  },
+  {
+    "revision": "e3049d2b893969fd9775",
+    "url": "/neil-solomon/csc212-music-playlist/static/js/runtime-main.c4081ac5.js"
+  },
+  {
+    "revision": "788f45ab87d5805ea498dade184b5cfc",
+    "url": "/neil-solomon/csc212-music-playlist/static/media/search.788f45ab.svg"
+  },
+  {
+    "revision": "a51ab7ed010a415170cd93a2a29d96dd",
+    "url": "/neil-solomon/csc212-music-playlist/static/media/youtube.a51ab7ed.svg"
+  }
+]);
